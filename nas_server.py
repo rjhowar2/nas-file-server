dd9f9b28a491e48c55610beddd67c994b402c692
rjhowar2 <rjhowar2@gmail.com>
2016-02-03 00:54:14 -0600

    commit changes to run from localhost

--- nas_server.py
+++ nas_server.py
@@ -35,4 +35,4 @@ def get_files():
 	return jsonify({'files': get_files_from_root(root)})
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="localhost")

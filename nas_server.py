8568aec0ff674a3b020c0ae29d9e2660a7f6bbcc
rjhowar2 <rjhowar2@gmail.com>
2016-02-08 08:29:47 -0600

    keeping all view logic in nas_server.py and adding a .gitignore

--- nas_server.py
+++ nas_server.py
@@ -1,38 +1,64 @@
 from flask import Flask, jsonify, request, abort
-import os
+from nas_utils import get_directory_contents
 
 app = Flask(__name__)
 
-def get_files_from_root(root):
-	all_files = {}
-	for path, subdirs, files in os.walk(root):
-		all_files['path'] = path
-		children = []
+BASE_URI = '/nas_server/api/v1.0'
 
-		def add_files(my_list, is_dir):
-			for name in my_list:
-				if not name.startswith("."):
-					children.append({'is_dir': is_dir, 'filename': name})
+@app.route('%s/directory' % BASE_URI, methods=['GET'])
+def directory_contents():
 
-		add_files(subdirs, True)
-		add_files(files, False)
+	path = request.args.get('path', None)
 
-		sorted_children = sorted(children, key=lambda x: x['filename'].lower())
-
-		all_files['children'] = sorted_children
-
-		break
-
-	return all_files
-
-@app.route('/nas_server/api/v1.0/files', methods=['POST'])
-def get_files():
-	if not request.json or not 'root' in request.json:
+	if not path:
 		abort(400)
 
-	root = request.json['root']
+	return jsonify({'files': get_directory_contents(path)})
 
-	return jsonify({'files': get_files_from_root(root)})
+@app.route('%s/files' % BASE_URI, methods=['GET'])
+def download_file():
+	path = request.args.get('path', None)
+
+	if not path:
+		abort(400)
+
+	return "Requested to download the following: %s" % path
+
+@app.route('%s/files' % BASE_URI, methods=['POST'])
+def upload_file():
+	required_args = ['filename','directory_path']
+	
+	if not request.json or not  all ([k in request.json for k in required_args]):
+		abort(400)
+
+	filename = request.json['filename']
+	dir_path = request.json['directory_path']
+
+	return "Request to upload %s to %s" % (filename, dir_path)
+
+@app.route('%s/files' % BASE_URI, methods=['PUT'])
+def update_file():
+	required_args = ['filename','directory_path', 'new_filename']
+	
+	if not request.json or not  all ([k in request.json for k in required_args]):
+		abort(400)
+
+	filename = request.json['filename']
+	dir_path = request.json['directory_path']
+	new_filename = request.json['new_filename']
+
+	return "Request to rename %s to %s" % (filename, new_filename)
+
+@app.route('%s/files' % BASE_URI, methods=['DELETE'])
+def delete_file():
+	path = request.args.get('path', None)
+
+	if not path:
+		abort(400)
+
+	return "Requested to delete the following: %s" % path
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="localhost")

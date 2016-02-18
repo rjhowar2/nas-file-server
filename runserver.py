ffd6248120f78a943ca40a5fd351128c3fdeae59
Ryan Howard <rhoward@Guaranteeds-MacBook-Air.local>
2016-02-18 12:03:52 -0600

    adding config change

--- runserver.py
+++ runserver.py
@@ -1,3 +1,22 @@
+import sys, getopt
 from nas_server import app
 
-app.run(debug=True, host="localhost")
+
+def main(argv):
+	host = "0.0.0.0"
+
+	try:
+		opts, args = getopt.getopt(argv,"lp:")
+   	except getopt.GetoptError:
+   		print 'runserver.py -l -p <port>'
+   		sys.exit(2)
+
+   	for opt, arg in opts:
+   		if opt == "-l":
+   			host = "localhost"
+
+	app.run(debug=True, host=host)
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])

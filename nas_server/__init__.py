bd4c6cf4f007ffbc35bb1d4a50f849d3394f4a49
Ryan Howard <rhoward@Guaranteeds-MacBook-Air.local>
2016-03-02 22:44:53 -0600

    changing start script to accept more options

--- nas_server/__init__.py
+++ nas_server/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
 app.config['allowed_filetypes'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp3', 'mp4'])
-app.config['base_directory'] = "%s/%s" % (os.path.dirname(os.path.realpath("nas_server/")), "nas_server/tests/test_files_dir")
 
 import nas_server.views
--- runserver.py
+++ runserver.py
@@ -1,22 +1,30 @@
-import sys, getopt
+import sys, getopt, os
 from nas_server import app
 
 
 def main(argv):
 	host = "0.0.0.0"
+	test_mode = False
+	base_dir = "%s/%s" % (os.path.dirname(os.path.realpath("nas_server/")), "nas_server/tests/test_files_dir")
+
 	try:
-		opts, args = getopt.getopt(argv,"lp:")
+		opts, args = getopt.getopt(argv,"tlp:d:")
 	except getopt.GetoptError:
-		print 'runserver.py -l -p <port>'
+		print 'runserver.py -t -l -p <port> -d <base_directory>'
 		sys.exit(2)
 
 	for opt, arg in opts:
 		if opt == "-l":
-			host = "localhost"
+			host = "127.0.0.1"
+		if opt == "-t":
+			test_mode = True
+		if opt == "-d":
+			base_dir = arg
 
-	app.config['test_mode'] = True
+	app.config['test_mode'] = test_mode
+	app.config['base_directory'] = base_dir
 	
-	app.run(debug=app.config['test_mode'], host=host)
+	app.run(debug=test_mode, host=host)
 
 
 if __name__ == "__main__":

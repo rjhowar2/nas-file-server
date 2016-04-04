1e2a73787bb16441952606de057a28ddc8ac403d
Ryan Howard <rhoward@Guaranteeds-MacBook-Air.local>
2016-04-04 15:12:11 -0500

    adding gunicorn deployment

--- nas_server/__init__.py
+++ nas_server/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from config import CLIENT_ID, CLIENT_SECRET
+from config import *
 
 app = Flask(__name__)
+app.debug=TEST_MODE
 
 app.config['allowed_filetypes'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp3', 'mp4', 'py', 'html'])
 app.config['SECRET_KEY'] = "No one ever really dies"
--- nas_server/config.py
+++ nas_server/config.py
@@ -1,3 +1,4 @@
 #Un-comment these and add the correct shared id and secret betwwen this app and the connected web app
 #CLIENT_ID = ""
 #CLIENT_SECRET = ""
+TEST_MODE=True
